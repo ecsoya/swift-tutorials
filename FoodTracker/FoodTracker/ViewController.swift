bbc5ffcd5127dbd17e2cd5ac68a5a58d03a8175c
Ecsoya <ecsoya@Ecsoyas-MacBook-Pro.local>
2016-12-13 16:10:09 +0800

    Update food tracker app

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    //MARK: Properties
+    @IBOutlet weak var defaultLabel: UILabel!
+    @IBOutlet weak var mealNameText: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+   
+    @IBOutlet weak var ratingControl: RatingControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mealNameText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK:Button actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        defaultLabel.text = "Hello Default 👍"
+    }
 
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        defaultLabel.text = mealNameText.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func selectImageFromLibrary(_ sender: UITapGestureRecognizer) {
+        mealNameText.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        imageView.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
 
